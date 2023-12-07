1d5ea9775682c9befce3d3c9a42aba28bb8ddd05
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2023-12-07 18:49:55 -0300

    auth context added

---
+++ src/api/auth.jsx
@@ -0,0 +1,7 @@
+// import axios from "./axios";
+
+export const registerRequest = async (user) => console.log(user);
+// axios.post(`/auth/register`, user);
+
+export const loginRequest = async (user) => console.log(user);
+// axios.post(`/auth/login`, user);
---
+++ src/context/authContext.jsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useState } from "react";
+import { loginRequest, registerRequest } from "../api/auth";
+
+const AuthContext = createContext();
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within a AuthProvider");
+  return context;
+};
+
+// eslint-disable-next-line react/prop-types
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [errors, setErrors] = useState([]);
+
+  const signup = async (user) => {
+    try {
+      const res = await registerRequest(user);
+      if (res.status === 200) {
+        setUser(res.data);
+        setIsAuthenticated(true);
+      }
+    } catch (error) {
+      console.log(error.response.data);
+      setErrors(error.response.data.message);
+    }
+  };
+
+  const signin = async (user) => {
+    try {
+      const res = await loginRequest(user);
+      setUser(res.data);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        signup,
+        signin,
+        isAuthenticated,
+        errors,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
