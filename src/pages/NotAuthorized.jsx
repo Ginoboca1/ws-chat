b350f4b5a86b040fff5604fb15ac9db5f9549b6c
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2023-12-16 10:39:10 -0300

    chat/ws

--- src/pages/NotAuthorized.jsx
+++ src/pages/NotAuthorized.jsx
@@ -1,18 +1,21 @@
-import { Button } from "../components/button";
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const UnauthorizedMessage = () => {
-  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h4 className="text-gray-700 text-2xl mb-4">
         Not authorized, please login to continue
       </h4>
-      <Button
-        className={"bg-blue-500 hover:bg-blue-700 px-4 py-1.5 rounded-sm"}
-        titleButton={"Login"}
-        clickAction={() => navigate("/")}
-      />
+
+      <button
+        className={
+          "bg-blue-500 hover:bg-blue-700 px-4 py-1.5 rounded-sm to-black"
+        }
+      >
+        <NavLink to="/register" className="to-black hover:underline mb-1">
+          Register
+        </NavLink>
+      </button>
     </div>
   );
 };
