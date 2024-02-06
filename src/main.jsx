17e93c3ba32a97269545409946dc631238d2e0e5
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2024-02-06 14:20:13 -0300

    normalize.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "normalize.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "../src/context/authContext.jsx";
